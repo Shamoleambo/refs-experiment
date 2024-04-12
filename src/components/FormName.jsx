fb5f0590929930d89a9f7a5dd8ec601a757c28df
Tiago Diniz Gomes <tidgomes@gmail.com>
2024-04-12 15:42:06 -0300

    create FormName component

---
+++ src/components/FormName.jsx
@@ -0,0 +1,9 @@
+export default function FormName() {
+  return (
+    <form>
+      <label htmlFor='name'>What's your name?</label>
+      <input type='text' id='name' name='name'></input>
+      <button>Ok</button>
+    </form>
+  )
+}
