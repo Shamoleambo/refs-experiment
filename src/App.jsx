057e06ee9c5ff1729ca30c10e54582efcd59b5ea
Tiago Diniz Gomes <tidgomes@gmail.com>
2024-04-12 15:45:32 -0300

    use refs to solve all my problems!

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,20 @@
+import { useState, useRef } from 'react'
+import FormName from './components/FormName'
+
 function App() {
-  return <></>
+  const [name, setName] = useState('<insert name>')
+  const nameInput = useRef()
+
+  function handleClick() {
+    setName(nameInput.current.value)
+  }
+
+  return (
+    <>
+      <h1>Hello {name}</h1>
+      <FormName ref={nameInput} onClick={handleClick} />
+    </>
+  )
 }
 
 export default App
--- src/components/FormName.jsx
+++ src/components/FormName.jsx
@@ -1,9 +1,15 @@
-export default function FormName() {
+import { forwardRef } from 'react'
+
+const FormName = forwardRef(function ({ onClick }, ref) {
   return (
     <form>
       <label htmlFor='name'>What's your name?</label>
-      <input type='text' id='name' name='name'></input>
-      <button>Ok</button>
+      <input ref={ref} type='text' id='name' name='name'></input>
+      <button type='button' onClick={onClick}>
+        Ok
+      </button>
     </form>
   )
-}
+})
+
+export default FormName
