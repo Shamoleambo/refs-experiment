2af311290a577ef96c90490601183bb1f4b5700d
Tiago Diniz Gomes <tidgomes@gmail.com>
2024-04-14 08:46:38 -0300

    change form button please? Yes sir!

--- src/components/FormName.jsx
+++ src/components/FormName.jsx
@@ -1,13 +1,14 @@
 import { forwardRef } from 'react'
+import Button from 'react-bootstrap/Button'
 
 const FormName = forwardRef(function ({ onClick }, ref) {
   return (
     <form>
       <label htmlFor='name'>What's your name?</label>
       <input ref={ref} type='text' id='name' name='name'></input>
-      <button type='button' onClick={onClick}>
+      <Button variant='dark' type='button' onClick={onClick}>
         Ok
-      </button>
+      </Button>
     </form>
   )
 })
