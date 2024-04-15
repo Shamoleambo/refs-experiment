7b7a3e03a005986467bced589e0f5f07127698a0
Tiago Diniz Gomes <tidgomes@gmail.com>
2024-04-15 17:08:39 -0300

    refactor Form component

--- src/components/FormName.jsx
+++ src/components/FormName.jsx
@@ -1,15 +1,23 @@
 import { forwardRef } from 'react'
+import Form from 'react-bootstrap/Form'
 import Button from 'react-bootstrap/Button'
 
 const FormName = forwardRef(function ({ onClick }, ref) {
   return (
-    <form>
-      <label htmlFor='name'>What's your name?</label>
-      <input ref={ref} type='text' id='name' name='name'></input>
+    <Form>
+      <Form.Group className='mb-3' controlId='formBasicEmail'>
+        <Form.Label>What's your name?</Form.Label>
+        <Form.Control
+          ref={ref}
+          type='text'
+          placeholder='Enter your name'
+        ></Form.Control>
+        <Form.Text className='text-muted'>What's up dog?</Form.Text>
+      </Form.Group>
       <Button variant='dark' type='button' onClick={onClick}>
         Ok
       </Button>
-    </form>
+    </Form>
   )
 })
 
